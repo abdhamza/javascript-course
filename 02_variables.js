d4104d54ec78a28ea38b66e3c05ebc86efc322da
Abdullah Hamza <engr.abdullah.hamza@gmail.com>
2023-06-15 17:24:31 +0500

    second class

---
+++ 02_variables.js
@@ -0,0 +1,19 @@
+// variables can store some information
+// that information can be used later
+
+// declaring a variable
+var firstName = "John"
+var secondName = "Doe"
+// use a variable
+console.log(firstName)
+console.log(secondName)
+// change value of variable
+var secondName = "carter"
+
+//combining static text with dynamic variable
+console.log("hello", firstName, secondName)
+
+//variable name cannot start with a number
+//only underscore _ and dollar $ sign are allowed to be used in variable name
+//we cannot use spaces in variable names
+// conventionally variable names in javascript are in CamelCase
