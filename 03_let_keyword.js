eacbbff1e3620f66771e495ac874649d54e56e1c
Abdullah Hamza <engr.abdullah.hamza@gmail.com>
2023-06-18 18:18:10 +0500

    second class

---
+++ 03_let_keyword.js
@@ -0,0 +1,10 @@
+//let keyword
+//let is also used to declare variable like var
+//let doesnot allow to modify value of variable
+let firstName = "john"
+// let firstName = "john Doe" //this will give error
+console.log(firstName)
+
+// we can change value of the variable by removing let tag from it 
+firstName = "john Doe"
+console.log(firstName)
---
+++ 04_const.js
@@ -0,0 +1,6 @@
+//declare constants
+const pi = 3.14;
+console.log(pi)
+
+pi = 4; //this will give error
+console.log(pi)
